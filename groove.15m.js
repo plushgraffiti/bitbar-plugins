26f9177d1c56260b4f61df798bf1041dbb7c6e93
Paul Griffiths <plush.graffiti@gmail.com>
2016-03-01 20:10:40 +0100

    Spacing fix

--- groove.15m.js
+++ groove.15m.js
@@ -20,8 +20,8 @@ function status(result) {
   } else if (result === 'opened') {
     return '♨ ';
   } else {
-  	return '◷ ';
-	}
+    return '◷ ';
+  }
 }
 
 function color(result) {
@@ -30,33 +30,33 @@ function color(result) {
   } else if (result === 'opened') {
     return 'orange';
   } else {
-		return 'black';
-	}
+    return 'black';
+  }
 }
 
 function report(ticket){
-	return "\n" + timeSince(ticket.updated_at) + ' - ' + ticket.summary + " | length=50";
+  return "\n" + timeSince(ticket.updated_at) + ' - ' + ticket.summary + " | length=50";
 }
 
 function statusIcon(states) {
-	if (disabled == true) {
-		return "Gr | color=gray font=Helvetica-Bold size=16"
-	}
+  if (disabled == true) {
+    return "Gr | color=gray font=Helvetica-Bold size=16"
+  }
 
-	if (states.indexOf("unread") >= 0) {
-		return "Gr | color=red font=Helvetica-Bold size=16"
-	} else if (states.indexOf("opened") >= 0) {
-		return "Gr | color=orange font=Helvetica-Bold size=16"
-	} else {
-		return "Gr | color=black font=Helvetica-Bold size=16"
-	}
+  if (states.indexOf("unread") >= 0) {
+    return "Gr | color=red font=Helvetica-Bold size=16"
+  } else if (states.indexOf("opened") >= 0) {
+    return "Gr | color=orange font=Helvetica-Bold size=16"
+  } else {
+    return "Gr | color=black font=Helvetica-Bold size=16"
+  }
 }
 
 function timeSince(timeStamp) {
-	// Expects timeStamp as a string, similar to: 2016-02-17T16:12:39Z
-	var timeStamp = new Date(Date.parse(timeStamp));
+  // Expects timeStamp as a string, similar to: 2016-02-17T16:12:39Z
+  var timeStamp = new Date(Date.parse(timeStamp));
   var now = new Date(),
-    secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
+  secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
   if(secondsPast < 60){
     return parseInt(secondsPast) + 's';
   }
@@ -75,17 +75,17 @@ function timeSince(timeStamp) {
 }
 
 function handleResponse(body) {
-	var states = [];
-	var output = '';
-	if (disabled == false) {
-		var old_state = '';
-	  var output = body.tickets.map(function(ticket){
-			states.push(ticket.state)
-			var spacer = (ticket.state == old_state) ? '' : '\n---\n';
-			old_state = ticket.state
-			return [spacer, status(ticket.state), ticket.title, ' | color=', color(ticket.state), ' length=50 href=https://getapp.groovehq.com/t/' + ticket.number, report(ticket)].join('');
-	  }).join('\n');
-	}
+  var states = [];
+  var output = '';
+  if (disabled == false) {
+    var old_state = '';
+    var output = body.tickets.map(function(ticket){
+      states.push(ticket.state)
+      var spacer = (ticket.state == old_state) ? '' : '\n---\n';
+      old_state = ticket.state
+      return [spacer, status(ticket.state), ticket.title, ' | color=', color(ticket.state), ' length=50 href=https://getapp.groovehq.com/t/' + ticket.number, report(ticket)].join('');
+    }).join('\n');
+  }
   console.log(statusIcon(states) + '\n---\n' + output);
 }
 
